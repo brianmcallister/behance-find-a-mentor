116b47e09b299d1c6ea93e1af89c0af4b159f209
Brian McAllister <brian@brianmcallister.com>
2012-11-30 11:19:25 -0500

    Start setting up the main index HTML file.

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,3 +1,40 @@
-<h2>Find A Mentor</h2>
+<!doctype html>
 
-<p>Improve your skillz!</p>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+
+<head>
+  <?php
+  $siteTitle = 'Advice — Powered by Behance';
+  $siteDescription = $siteTitle;
+  $siteKeywords = $siteTitle;
+  $siteUrl = isset($_SERVER['HTTPS']) ? 'https://' : 'http://' . $_SERVER['HTTP_HOST'];
+  ?>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title><?php echo $siteTitle; ?></title>
+  <meta name="description" content="<?php echo $siteTitle; ?>">
+  <meta name="keywords" content="<?php echo $siteTitle; ?>">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+  <link rel="stylesheet" href="assets/css/style.css">
+    
+  <script data-main="assets/js/main.js" src="assets/js/libs/require.js"></script>
+</head>
+
+<body>
+  <h2>Find A Mentor</h2>
+  <p>Improve your skillz!</p>
+  
+  <script>
+    var _gaq=[['_setAccount','UA-XXX'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+  </script>
+</body>
+</html>
