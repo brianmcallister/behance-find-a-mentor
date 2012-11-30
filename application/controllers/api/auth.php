ad0b5af49ea138c0f4a676b235eba06c18db59b9
Matt LeBrun <matt@behance.com>
2012-11-30 16:41:46 -0500

    Got Auth working.

---
+++ application/controllers/api/auth.php
@@ -0,0 +1,64 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Auth extends MY_Controller {
+
+  public function index() {
+
+    $this->load->database();
+    $this->load->spark('curl/1.2.1');
+
+    // get's POST data and santizes to prevent XSS (NOT DB SANITIZING)
+    $code = $this->input->post( 'code' , TRUE );
+
+    $response = $this->curl->simple_post('https://www.behance.net/v2/oauth/token', array(
+      'client_id'     => BEHANCE_API_KEY,
+      'client_secret' => BEHANCE_API_SECRET,
+      'redirect_uri'  => BEHANCE_API_REDIRECT,
+      'code'          => $code,
+      'grant_type'    => 'authorization_code'
+    ) );
+
+    // -- FOR DEBUGGING --
+    // print_r( $this->curl->info );
+
+    $response = json_decode( $response, true );
+    $token    = $response['access_token'];
+
+    $response          = $response['user'];
+    $response['token'] = $token;
+
+    // $response  = $this->_sanitize( $response );
+    $user_data = $response;
+
+
+    // get images and fields
+    $image  = $response['images'];
+    $fields = $response['fields'];
+    unset(
+      $response['images'],
+      $response['fields']
+    );
+
+    // print_r( $response );
+
+    return $this->_json_output( array(
+      'valid'  => 1,
+      'status' => 200,
+      'user'   => $user_data
+    ) );
+
+  } // index
+
+  protected function _sanitize( $data ) {
+
+    if ( empty( $data ) )
+      return $data;
+
+    foreach ( $data as $key => $val )
+      $data[ $key ] = $this->db->escape( $val );
+
+    return $data;
+
+  } // _sanitize
+
+} // Auth
--- assets/js/views/welcomeView.js
+++ assets/js/views/welcomeView.js
@@ -3,39 +3,39 @@
 
 /*
   Welcome view.
-  
+
   The first view a user sees when visiting the site.
 */
 define([
   // Libs
   'jquery',
-  
+
   // Base views.
   'baseviews/baseView',
-  
+
   // Templates
   'text!templates/welcome.html'
 ], function ($, BaseView, welcomeTemplate) {
   'use strict';
-  
+
   return BaseView.extend({
     events: {
       'click .sign-in': 'signIn',
       'click .sign-up': 'signUp'
     },
-    
+
     template: _.template(welcomeTemplate),
-    
+
     authUrl: 'https://www.behance.net/v2/oauth/authenticate',
     redirectUrl: window.location.href + '#auth',
-    
+
     initialize: function () {
       this.$el.find('.container').html(this.template());
     },
-    
+
     signIn: function (event) {
       event.preventDefault();
-      
+
       // Redirect the user to authenticate.
       window.location.href = this.authUrl + '?' + $.param({
         'client_id': window.app.config.api_key,
@@ -44,10 +44,10 @@ define([
         'state': 'asdfq234'
       });
     },
-    
+
     signUp: function (event) {
       event.preventDefault();
-      
+
       console.log('sign up')
     }
   })
